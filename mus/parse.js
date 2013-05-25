f9c4dec59f6184e07b6b7b2de75ed4d65a6c5ecf
James Cartledge <jcartledge@gmail.com>
2013-05-25 22:00:31 +1000

    document syntax

--- mus/parse.js
+++ mus/parse.js
@@ -5,6 +5,15 @@ var fs = require('fs');
 // Create my parser
 var parse = PEG.buildParser(fs.readFileSync('mus.peg', 'utf-8')).parse;
 
+/**
+ * Notes are `[note][octave]:[duration (ms)]`.
+ * Rests are `_:[duration (ms)]`.
+ * Notes are separated by optional whitespace or commas.
+ * Chords are joined with ampersands (&).
+ * Whitespace and commas are not required, but can be used anywhere between 
+ * notes and rests to enhance readability.
+ */
+
 // Do a test
 assert.deepEqual(
   parse('_:100,a1:100 & d#1:100 & c3:50, _:40'),
