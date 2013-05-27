284006c21033aaf35ccf29fe70c10c577c056b24
James Cartledge <jcartledge@gmail.com>
2013-05-27 21:31:52 +1000

    begin-test fixed

--- scheem/begin-test.js
+++ scheem/begin-test.js
@@ -1,9 +1,10 @@
-var scheem = require("./eval.js");
+var evalScheem = require("./eval.js");
 
-var env = {a: 2, b: 7};
+var env = {'bindings': {'a': 2, 'b': 7}};
 var prg = ['begin',
-            ['define', 'x', 5],
-            ['set!', 'x', ['+', 'x', 1]],
-            ['+', 2, 'x']];
-var result = scheem(prg, env);
-console.log(result);
+  ['define', 'x', 5],
+  ['set!', 'x', ['+', 'x', 1]],
+  ['+', 2, 'x']
+];
+
+console.log(evalScheem(prg, env));
